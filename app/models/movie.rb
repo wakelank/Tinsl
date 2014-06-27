1cf8127eb6baa76e49358ae351f2e11b567ef82b
Aldric Giacomoni <trevoke@gmail.com>
2014-06-27 16:28:35 -0400

    Reindent and breaking change (shared/_header)

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,24 +5,26 @@ class Movie < ActiveRecord::Base
   has_many :actors_movies
   has_many :actors, :through => :actors_movies
 
-  def get_total_gross
+  def get_total_gross # You mean the 'Ew' factor?
     total = 0
-    self.actors.each do |actor| 
-      total += actor.value 
+    self.actors.each do |actor|
+      total += actor.value
     end
     total
   end
 
+  # Dang that's a long method. I'd create a class called 'MovieRanking'
+  # the only responsibility of which is to do THIS.
   def ranking
     ranking = {}
     rank = 0
     arr = []
-  
+
     Seedmovie.all.each do |movie|
       arr << movie
     end
     arr.sort! {|x, y| y[:gross] <=> x[:gross] }
-  
+
     arr.each do |movie|
       if self.get_total_gross > movie.gross
         rank = arr.index(movie)+1
@@ -39,24 +41,24 @@ class Movie < ActiveRecord::Base
       one_higher_rank = ""
       one_lower_title = arr[rank].title
       one_lower_rank = "#" + (rank + 1).to_s
-    # elsif rank == 0
-    #   one_higher_title = arr[rank].title
-    #   one_higher_rank = "#" + (rank).to_s
-    #   one_lower_rank = ""
-    #   one_lower_title = ""
+      # elsif rank == 0
+      #   one_higher_title = arr[rank].title
+      #   one_higher_rank = "#" + (rank).to_s
+      #   one_lower_rank = ""
+      #   one_lower_title = ""
     else
       one_higher_title = arr[rank - 2].title
-      one_higher_rank = "#" + (rank-1).to_s + ": " 
+      one_higher_rank = "#" + (rank-1).to_s + ": "
       one_lower_title = arr[rank-1].title
-      one_lower_rank = "#" + (rank + 1).to_s + ": " 
+      one_lower_rank = "#" + (rank + 1).to_s + ": "
     end
 
     #one_lower = arr[rank].title
-    ranking = { :rank => "#" + (rank).to_s, 
-                :one_higher_title => one_higher_title, 
-                :one_lower_title => one_lower_title,
-                :one_higher_rank => one_higher_rank,
-                :one_lower_rank => one_lower_rank.to_s  }
+    ranking = { :rank => "#" + (rank).to_s,
+               :one_higher_title => one_higher_title,
+               :one_lower_title => one_lower_title,
+               :one_higher_rank => one_higher_rank,
+               :one_lower_rank => one_lower_rank.to_s  }
   end
 
   # def get_seedmovie(rank)
