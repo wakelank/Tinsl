661e3db994bacbb1c36c04a6f155c5c9542b43c6
wakelank <wakelank@gmail.com>
2015-07-21 11:25:46 -0400

    adds guest login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,28 @@
 class SessionsController < ApplicationController
 
   def new
-
   end
 
   def create
-    user = User.find_by(email: params[:email])
+      if params[:guest] then
+          email = "guest@guest.com"
+          password = "password"
+      else
+          email = params[:email]
+          password = params[:password]
+      end
+      user = User.find_by(email: email)
 
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id 
-      redirect_to movies_path, notice: "Signed in as #{user.name}."
-    else
-      redirect_to log_in_path, alert: "Login failed."
-    end
-
-
+      if user && user.authenticate(password)
+        session[:user_id] = user.id 
+        redirect_to movies_path, notice: "Signed in as #{user.name}."
+      else
+        redirect_to log_in_path, alert: "Login failed."
+      end
   end
 
   def destroy
-    session[:user_id] = nil
-
-    redirect_to log_in_path, notice: "Logged out."
-
-
+      session[:user_id] = nil
+      redirect_to log_in_path, notice: "Logged out."
   end
-
-
 end
